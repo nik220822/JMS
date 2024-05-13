3f3a76e421235be8343e8d3b94df136bbc90bb43
Nikolai Pavlov <nik220822@rambler.ru>
2024-05-13 23:29:53 +0300

    forgot to save addition of the sender Now did

--- src/main/java/NicMqStub.java
+++ src/main/java/NicMqStub.java
@@ -9,6 +9,7 @@ import com.ibm.mq.jms.MQQueue;
 import com.ibm.mq.jms.MQQueueConnection;
 import com.ibm.mq.jms.MQQueueConnectionFactory;
 import com.ibm.mq.jms.MQQueueReceiver;
+import com.ibm.mq.jms.MQQueueSender;
 import com.ibm.mq.jms.MQQueueSession;
 
 public class NicMqStub {
@@ -25,13 +26,23 @@ public class NicMqStub {
 			connection = (MQQueueConnection) factory.createQueueConnection();
 			session = (MQQueueSession) connection.createQueueSession(true, Session.AUTO_ACKNOWLEDGE);
 			MQQueue in = (MQQueue) session.createQueue("Mq.IN");
+			MQQueue out = (MQQueue) session.createQueue("Mq.OUT");
 			receiver = (MQQueueReceiver) session.createReceiver(in);
+			MQQueueSender sender = (MQQueueSender) session.createSender(out);
 			javax.jms.MessageListener listener = new javax.jms.MessageListener() {
 
 				@Override
 				public void onMessage(Message m) {
 					// TODO Auto-generated method stub
 					System.out.println("The message was cought.");
+					try {
+						sender.send(m);
+					} catch (JMSException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("The message was not sent.");
+					}
+					System.out.println("The message was sent.");
 					if (m instanceof TextMessage) {
 						try {
 							TextMessage tm = (TextMessage) m;
